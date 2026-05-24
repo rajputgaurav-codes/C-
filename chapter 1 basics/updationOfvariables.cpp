f3919c40958d14f05b646943df68715f5c15aeb5
gauravsingh22-del <gr0757931@gmail.com>
2026-05-24 11:10:21 +0530

    print numbers using updation

---
+++ chapter 1 basics/updationOfvariables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int x=5;        //declaration value of x is 5
+  cout<<x<<endl;
+  x = x + 7;      //Now,value of x is updated.
+  cout<<x<<endl;
+  x *= 5;         //Now, Again value of x is updated.
+  cout<<x<<endl;  
+  float y=5;      //declare value of y in float data type they  show             the decimal point
+  cout<<y<<endl;
+  y /= 2;
+  cout<<y;
+}
