62dff1d9581f1597bf92296c1bf9f6f44aff6288
gauravsingh22-del <gr0757931@gmail.com>
2026-05-24 11:10:59 +0530

    print numbers using variables

---
+++ chapter 1 basics/variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int x=5,y=10; //declaration with initialization 
+   cout<<x<<endl;
+   cout<<y<<endl;
+  int z; //declaration
+  z = x*y;
+  cout<<z;
+
+}
