4d8a816ef30d83c803c8f153b2cdc5017ff80453
gauravsingh22-del <gr0757931@gmail.com>
2026-06-04 15:06:14 +0530

    calculating simple interest.

---
+++ Chapter 1 practice program/2.cpp
@@ -0,0 +1,10 @@
+//Calculating simple interest.
+#include<iostream>
+using namespace std;
+int main(){
+  float p,r,t;
+  p=25.65 , r=78 , t=5;
+  float si;
+  si = (p*r*t)/100;
+  cout<<"simple interest = "<<si;
+}
