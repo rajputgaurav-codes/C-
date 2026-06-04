a47c9bb77a28e5ae675245ef4e6f243834dccb51
gauravsingh22-del <gr0757931@gmail.com>
2026-06-04 15:05:27 +0530

    calculating area of circle

---
+++ Chapter 1 practice program/1.cpp
@@ -0,0 +1,11 @@
+//Calculating area of circle.
+
+#include<iostream>
+using namespace std;
+int main(){
+  int r=2;
+  float area_of_circle;
+  area_of_circle = 3.14 * r * r ;
+  cout<<"Area of Circle = "<<area_of_circle;
+
+}
