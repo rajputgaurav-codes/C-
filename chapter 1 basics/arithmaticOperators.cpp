d1902343d086206351d617d18efc48318575e77c
gauravsingh22-del <gr0757931@gmail.com>
2026-06-27 11:26:27 +0530

    arithmatic operator

---
+++ chapter 1 basics/arithmaticOperators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x=5 , y=3;
+    cout<<x+y<<endl;  //excepted ans : 8
+    cout<<x-y<<endl;  //excepted ans : 2
+    cout<<x*y<<endl;  //excepted ans : 15
+    cout<<x/y<<endl;  //excepted ans : 1.666     but answer is:  1    because this is integer value not a float value
+
+
+    // you want to get a real ans to difine value in the float.
+ 
+}
