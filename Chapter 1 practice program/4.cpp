b10f44b90a531c097d5386587217a6dc7309ace8
gauravsingh22-del <gr0757931@gmail.com>
2026-06-29 15:51:05 +0530

    Take input from the user and print the sum

---
+++ Chapter 1 practice program/4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+    // print the sum of both number and take input from user of those both numbers.
+    
+    int x,y;
+    cout<<"enter fist number: ";
+    cin>>x;
+    cout<<"enter second number: ";
+    cin>>y;
+    cout<<"The sum of both number is: "<<x+y;
+ }
