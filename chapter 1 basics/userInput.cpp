375259ba082c3912ad413ecd69fdaa185eb5d7d2
gauravsingh22-del <gr0757931@gmail.com>
2026-06-29 15:49:38 +0530

    learning how to get input from the user

---
+++ chapter 1 basics/userInput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter a number: "; 
+    
+    cin>>x;          //cin is use to take input from the user . the >> use in input and output ke liye <<.
+
+
+    cout<<"The square of this number is: ";
+    
+    cout<<x*x;
+}
